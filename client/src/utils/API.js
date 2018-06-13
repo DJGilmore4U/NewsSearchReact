8d634814a63241fa3b1e846ee14dd9fb11d097b9
Customer_Support_1 <daniellejgilmore@gmail.com>
2018-06-12 19:21:35 -0700

    adjusting the API values

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const URL ='https://api.nytimes.com/svc/search/v2/articlesearch.json';
-const KEY = 'e167471ecd274f53962769e06c849f2b';
+//const KEY = 'e167471ecd274f53962769e06c849f2b';
+const KEY = 'f1f91dab1e7443d5bb6adc919e7f9075';
 
 export default {
 	getData(search){
