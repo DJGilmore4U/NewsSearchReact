2339864f1f52bfea4c6eb872f7374d886f87ee54
Customer_Support_1 <daniellejgilmore@gmail.com>
2018-06-12 19:47:36 -0700

    more

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+const URL ='https://api.nytimes.com/svc/search/v2/articlesearch.json';
+//const KEY = 'e167471ecd274f53962769e06c849f2b';
 const KEY = 'f1f91dab1e7443d5bb6adc919e7f9075';
 
 export default {
@@ -11,7 +12,7 @@ export default {
 		return axios.get("/api/articles");
 	},
 	saveArticles(Article){
-		return axios.post("api/articles", Article);
+		return axios.post("api/articles", Article)
 	},
 	deleteArticle(id) {
 		return axios.delete("api/articles/" + id);
