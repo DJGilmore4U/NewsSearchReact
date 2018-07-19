04d73658c2d7e1b3b3d831a1d55d674628b32e4c
Customer_Support_1 <daniellejgilmore@gmail.com>
2018-07-19 14:22:54 -0700

    new kinder build2

--- client/src/components/Search/Button.js
+++ client/src/components/Search/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
  const Button = (props) => {
  	return(
- 		<button onClick={props.handleButtonClick} className='btn btn-danger btn-lg'>Search</button>
+ 		<button onClick={props.handleButtonClick} className='btn btn-info btn-lg'>Search</button>
  		)	
  };
 
